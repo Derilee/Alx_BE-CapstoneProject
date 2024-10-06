2328757f01ffbd37670d2cef3b56dc3b2d12514e
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-10-06 10:14:44 +0100

    new changes

--- Inventory_Management_API/users/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import RegisterView, ProfileView, LoginView, LogoutView
-from django.urls import path
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
-]
