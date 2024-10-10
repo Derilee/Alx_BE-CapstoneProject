68195854f93cfb54ccbf290b5ea0ce239acb6274
Samuel Victor <35655887+Derilee@users.noreply.github.com>
2024-10-11 00:09:15 +0100

    BE capstone project - Building an Inventory Management API

--- inventory-management-api/inventory/serializers.py
+++ inventory-management-api/inventory/serializers.py
@@ -17,10 +17,16 @@ class InventoryItemSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Price can't be a negative value") 
         return value
 
+#serializes the fields i want to display when viewing an inventory history
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItem
+        fields = ['name', 'description', 'price', 'category']
 
 #Serializes the Inventory Change History model
 class InventoryChangeHistorySerializer(serializers.ModelSerializer):
-    changed_by = UserSerializer(read_only=True) #uses UserSerializer to display the details of the user who made the change
+    changed_by = UserSerializer(read_only=True) #uses UserSerializer to display the details of who made the change
+    item = ItemSerializer(read_only=True) #uses ItemSerializer to show the item that has been changed
     class Meta:
         model = InventoryChangeHistory
         fields = ['item', 'old_quantity', 'new_quantity', 'change_date', 'changed_by']
